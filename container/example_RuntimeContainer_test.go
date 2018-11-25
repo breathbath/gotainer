1286b423e1e5cc84d0be43412e33a022b21e2e07
Andrey P <posandrey@gmail.com>
2018-11-25 23:24:42 +0100

    Further examples for new method, constructor

--- container/example_RuntimeContainer_test.go
+++ container/example_RuntimeContainer_test.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-// This example fetching of services from container with a wrapped error
+// This example scanning of services from container with a possible error output
 // if it happens
 func ExampleRuntimeContainer_ScanSecure() {
 	cont := NewRuntimeContainer()
@@ -30,4 +30,86 @@ func ExampleRuntimeContainer_ScanSecure() {
 	// <nil>
 	// Some error [check 'parameterFailed' service]
 	// Unknown dependency 'unknownParameter'
+}
+
+// This example shows how to add a custom constructor and use an existing service in it
+func ExampleRuntimeContainer_AddConstructor() {
+	cont := NewRuntimeContainer()
+	cont.AddConstructor("secretKey", func(c Container) (interface{}, error) {
+		return "00000", nil
+	})
+	cont.AddConstructor("login", func(c Container) (interface{}, error) {
+		return "admin", nil
+	})
+
+	cont.AddConstructor("credentials", func(c Container) (interface{}, error) {
+		secretKey := c.Get("secretKey", true).(string)
+		login := c.Get("login", true).(string)
+
+		return fmt.Sprintf("%s:%s", login, secretKey), nil
+	})
+
+	credentials := cont.Get("credentials", true).(string)
+	fmt.Println(credentials)
+
+	// Output:
+	// admin:00000
+}
+
+type Db struct{}
+
+func NewDb() Db {
+	return Db{}
+}
+
+type NameProvider struct {
+	db Db
+}
+
+func (db Db) GetName() string {
+	return "Elton John"
+}
+
+func NewNameProvider(db Db) NameProvider {
+	return NameProvider{db: db}
+}
+
+func (dp NameProvider) GetFullName(salutation string) string {
+	return salutation + " " + dp.db.GetName()
+}
+
+// This example shows how to add a struct constructor with some dependencies
+func ExampleRuntimeContainer_AddNewMethod() {
+	//having declared struct dependencies:
+	//type Db struct{}
+	//
+	//func NewDb() Db {
+	//	return Db{}
+	//}
+	//
+	//type NameProvider struct {
+	//	db Db
+	//}
+	//
+	//func (db Db) GetName() string {
+	//	return "Elton John"
+	//}
+	//
+	//func NewNameProvider(db Db) NameProvider {
+	//	return NameProvider{db: db}
+	//}
+	//
+	//func (dp NameProvider) GetFullName(salutation string) string {
+	//	return salutation + " " + dp.db.GetName()
+	//}
+
+	cont := NewRuntimeContainer()
+	cont.AddNewMethod("db", NewDb)
+	cont.AddNewMethod("nameProvider", NewNameProvider, "db")
+
+	nameProvider := cont.Get("nameProvider", true).(NameProvider)
+	fmt.Println(nameProvider.GetFullName("Mr"))
+
+	// Output:
+	// Mr Elton John
 }
